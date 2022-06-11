d3f95cebaca566d4ed73d9ad357b8a3956f3ef59
junggyun-choi <junggyun.choi@webtoonscorp.com>
2022-06-11 14:08:04 +0900

    initial commit

---
+++ src/test/java/mashup/spring/jsmr/JsmrApplicationTests.java
@@ -0,0 +1,13 @@
+package mashup.spring.jsmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsmrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
