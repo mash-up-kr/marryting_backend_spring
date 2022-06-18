d18d292bdd955101dd7abc19e2b2322a40050226
junggyun-choi <junggyun.choi@webtoonscorp.com>
2022-06-18 14:45:18 +0900

    feat: querydsl setting

---
+++ jsmr-domain/src/main/java/mashup/spring/jsmr/config/QuerydslConfig.java
@@ -0,0 +1,24 @@
+package mashup.spring.jsmr.config;
+
+//import com.querydsl.jpa.impl.JPAQueryFactory;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//
+//import javax.persistence.EntityManager;
+//import javax.persistence.PersistenceContext;
+
+//@EnableJpaAuditing
+//@Configuration
+//public class QuerydslConfig {
+//
+//    @PersistenceContext
+//    private EntityManager entityManager;
+//
+//    @Bean
+//    public JPAQueryFactory jpaQueryFactory() {
+//        return new JPAQueryFactory(entityManager);
+//    }
+//
+//}
+
