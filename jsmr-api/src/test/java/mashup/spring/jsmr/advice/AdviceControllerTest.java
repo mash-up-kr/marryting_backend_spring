f56ce1eb06bd73ad79b80df6098744bcba9cacc4
Jaemin.Park <once29@naver.com>
2022-06-18 16:59:21 +0900

    fix: remove unnecessary file

--- jsmr-api/src/test/java/mashup/spring/jsmr/advice/AdviceControllerTest.java
+++ jsmr-api/src/test/java/mashup/spring/jsmr/advice/AdviceControllerTest.java
@@ -13,6 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Disabled
 @WebMvcTest(AdviceController.class)
 public class AdviceControllerTest {
 
