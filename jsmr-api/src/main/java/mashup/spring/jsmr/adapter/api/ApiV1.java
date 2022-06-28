e352f860a74ba3876d346894ad857ed932a7d4be
Jaemin.Park <once29@naver.com>
2022-06-28 12:43:22 +0900

    feat: implement ApiV1

---
+++ jsmr-api/src/main/java/mashup/spring/jsmr/adapter/api/ApiV1.java
@@ -0,0 +1,9 @@
+package mashup.spring.jsmr.adapter.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public @interface ApiV1 {
+}
