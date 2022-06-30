925502c42539088f5230d5997878c2781708d49e
Jaemin.Park <once29@naver.com>
2022-06-30 12:31:14 +0900

    feat: remove ApiV1

--- jsmr-api/src/main/java/mashup/spring/jsmr/adapter/api/ApiV1.java
+++
@@ -1,9 +0,0 @@
-package mashup.spring.jsmr.adapter.api;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/v1")
-public @interface ApiV1 {
-}
