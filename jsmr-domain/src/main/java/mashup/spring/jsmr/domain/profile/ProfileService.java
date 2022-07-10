6bcd4943151e82fb8635e9bb9cbf6abdbeddb226
SungMin Joo <tjdals3415@gmail.com>
2022-07-10 22:00:47 +0900

    etc: remove unnecessary code

--- jsmr-domain/src/main/java/mashup/spring/jsmr/domain/profile/ProfileService.java
+++ jsmr-domain/src/main/java/mashup/spring/jsmr/domain/profile/ProfileService.java
@@ -1,22 +1,15 @@
 package mashup.spring.jsmr.domain.profile;
 
 import lombok.RequiredArgsConstructor;
-import mashup.spring.jsmr.domain.answer.Answer;
-import mashup.spring.jsmr.domain.answer.AnswerRepository;
 import mashup.spring.jsmr.domain.exception.EntityNotFoundException;
 import mashup.spring.jsmr.domain.keyword.Keyword;
 import mashup.spring.jsmr.domain.keyword.KeywordRepository;
 import mashup.spring.jsmr.domain.question.QuestionRepository;
 import mashup.spring.jsmr.domain.question.Questionnaire;
-import mashup.spring.jsmr.domain.user.Gender;
-import mashup.spring.jsmr.domain.user.User;
-import mashup.spring.jsmr.domain.user.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -27,8 +20,6 @@ public class ProfileService {
     private final ProfileRepository profileRepository;
     private final QuestionRepository questionRepository;
     private final KeywordRepository keywordRepository;
-    private final UserRepository userRepository;
-    private final AnswerRepository answerRepository;
 
     public List<Profile> getDetailProfile(final Long userId, final Long profileId) {
         Profile profile = profileRepository.findById(profileId).orElseThrow(EntityNotFoundException::new);
