3ef4dee8a44cd4a8174dc941bbcac0cd26c6f23a
junggyun-choi <junggyun.choi@webtoonscorp.com>
2022-07-26 23:48:21 +0900

    feat: cors add

--- jsmr-api/src/main/java/mashup/spring/jsmr/adapter/infrastructure/config/WebMvcConfig.java
+++ jsmr-api/src/main/java/mashup/spring/jsmr/adapter/infrastructure/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import mashup.spring.jsmr.adapter.infrastructure.interceptor.LoginUserIdArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,5 +18,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserIdArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
     }
 }
