37bf7ac56c638050deb16e8334a8628e8afde060
junggyun-choi <junggyun.choi@webtoonscorp.com>
2022-08-20 15:30:24 +0900

    refactor: s3 upload

--- jsmr-domain/src/main/java/mashup/spring/jsmr/domain/file/FileUploader.java
+++ jsmr-domain/src/main/java/mashup/spring/jsmr/domain/file/FileUploader.java
@@ -1,13 +1,9 @@
 package mashup.spring.jsmr.domain.file;
 
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 
 public interface FileUploader {
 
-    String upload(InputStream inputStream, String originFileName, String fileSize) throws IOException;
-    List<String> multiUpload(MultipartFile[] multipartFiles, String fileSize) throws IOException;
+    String upload(InputStream inputStream, String fileSize) throws IOException;
 }
