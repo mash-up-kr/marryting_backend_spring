ffaad48ffc28d940100fc049e7a5a00c7fc9bd34
Jaemin.Park <once29@naver.com>
2022-08-21 03:47:28 +0900

    refactor: remove unnecessary api

--- jsmr-api/src/main/java/mashup/spring/jsmr/adapter/api/profile/ProfileController.java
+++ jsmr-api/src/main/java/mashup/spring/jsmr/adapter/api/profile/ProfileController.java
@@ -3,10 +3,7 @@ package mashup.spring.jsmr.adapter.api.profile;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import mashup.spring.jsmr.adapter.api.ApiResponse;
-import mashup.spring.jsmr.adapter.api.profile.dto.CreateProfileRequestDTO;
-import mashup.spring.jsmr.adapter.api.profile.dto.CreateProfileResponseDTO;
 import mashup.spring.jsmr.adapter.api.profile.dto.ProfileDetailResponseDTO;
-import mashup.spring.jsmr.adapter.infrastructure.interceptor.LoginUserId;
 import mashup.spring.jsmr.application.profile.ProfileApplicationService;
 import mashup.spring.jsmr.domain.file.FileUploader;
 import org.springframework.http.HttpStatus;
@@ -14,12 +11,10 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import springfox.documentation.annotations.ApiIgnore;
 
 import java.io.IOException;
 
@@ -33,18 +28,9 @@ public class ProfileController {
 
     @ApiOperation("프로필 상세 조회")
     @GetMapping("/{profileId}/detail")
-    public ApiResponse<ProfileDetailResponseDTO> getProfileDetail(@ApiIgnore @LoginUserId Long userId,
-                                                                  @PathVariable Long profileId) {
+    public ApiResponse<ProfileDetailResponseDTO> getProfileDetail(@PathVariable Long profileId) {
         return ApiResponse.success(HttpStatus.OK, profileApplicationService.getDetailProfileById(profileId));
     }
-
-    @ApiOperation("프로필 생성")
-    @PostMapping
-    public ApiResponse<CreateProfileResponseDTO> createProfile(@ApiIgnore @LoginUserId Long userId,
-                                                               @RequestBody CreateProfileRequestDTO createProfileRequestDTO) {
-        return ApiResponse.success(HttpStatus.CREATED, profileApplicationService.createProfile(userId, createProfileRequestDTO));
-    }
-
 
     @ApiOperation("프로필 이미지 업로드 multipart/form-data")
     @PostMapping(value = "/image",
