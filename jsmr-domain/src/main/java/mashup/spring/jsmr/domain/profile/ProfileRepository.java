79d2e87fd02a9372b8bab979f49d62560f65531c
jeongmin-han <jeongmin.han@navercorp.com>
2022-08-21 00:50:15 +0900

    feat : update jpql -> querydsl & 소개팅참여자 조회 weddingId Param 추가 & update Profile Optional

--- jsmr-domain/src/main/java/mashup/spring/jsmr/domain/profile/ProfileRepository.java
+++ jsmr-domain/src/main/java/mashup/spring/jsmr/domain/profile/ProfileRepository.java
@@ -3,17 +3,12 @@ package mashup.spring.jsmr.domain.profile;
 import mashup.spring.jsmr.domain.profile.custom.ProfileCustomRepository;
 import mashup.spring.jsmr.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long>, ProfileCustomRepository {
     
     Optional<Profile> findAllByUserId(Long userId);
-
-    @Query("select p from Profile p join fetch p.user u where u.id = :userId")
-    Optional<Profile> findByUserId(@Param("userId") Long userId);
 
     boolean existsProfileByUser(User user);
 }
