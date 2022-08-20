b386b9651bb4ed533ac88691f6cc8a7849daff77
SungMin Joo <tjdals3415@gmail.com>
2022-08-20 16:13:27 +0900

    etc: set access level in age util func

--- jsmr-api/src/main/java/mashup/spring/jsmr/adapter/util/AgeUtil.java
+++ jsmr-api/src/main/java/mashup/spring/jsmr/adapter/util/AgeUtil.java
@@ -1,9 +1,14 @@
 package mashup.spring.jsmr.adapter.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.experimental.UtilityClass;
 import mashup.spring.jsmr.domain.profile.Profile;
 
 import java.time.LocalDate;
 
+@UtilityClass
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AgeUtil {
     public static int CalculateUserAge(String birth) {
         return LocalDate.now().getYear() - Integer.parseInt(birth.substring(0,4)) + 1;
